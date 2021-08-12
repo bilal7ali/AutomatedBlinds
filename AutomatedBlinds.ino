966a28101c28b13d42d3d9d36201dd6a46c1d64d
bilal7ali <80595485+bilal7ali@users.noreply.github.com>
2021-08-11 21:52:08 -0400

    Upgraded motors and drivers from 28BYJ-48/ULN2003 to NEMA17/A4988 for faster speed and increased torque.

--- AutomatedBlinds.ino
+++ AutomatedBlinds.ino
@@ -1,52 +1,59 @@
-//Include the Arduino Stepper Library
-#include <Stepper.h>
 #include <IRremote.h>
 
 //CONSTANTS
-//Steps per internal motor revolution
-const float STEPS_PER_REV = 32;
-
+const int dirPin = 2;  // Direction
+const int stepPin = 3; // Step
+const int STEPS_PER_REV = 200; // Steps per rotation
 const int irReceiverPin = 7;
+
 IRrecv irrecv(irReceiverPin);
 decode_results results;
 
-//Amount of Gear Reduction
-const float GEAR_RED = 64;
-
-//Steps per geared output rotation
-const float STEPS_PER_OUT_REV = STEPS_PER_REV * GEAR_RED;
-
 //VARIABLES
-int StepsRequired;
 int LED = 6;
-
-//Create Instance of Stepper Class
-//Pins used are 8,9,10,11 connceted to ULN2003 Motor Driver In1, In2, In3, In4
-//Pins entered in sequence 1-3-2-4 for proper step sequencing
-
-Stepper steppermotor(STEPS_PER_REV, 8, 10, 9, 11);
 
 void setup() {
   pinMode(LED, OUTPUT);
+  pinMode(stepPin, OUTPUT);
+  pinMode(dirPin, OUTPUT);
   irrecv.enableIRIn();
 }
 
 void loop() {
+  //Receives IR signal from remote, decodes it and enters the correct case to turn CW or CCW
   if (irrecv.decode(&results)) {
     long int decCode = results.value;
     switch (results.value) {
+      //Twist Blinds Open
       case 16753245:
       digitalWrite(LED, HIGH);
-      StepsRequired = STEPS_PER_OUT_REV / 2;
-      steppermotor.setSpeed(400);
-      steppermotor.step(StepsRequired);
+      
+      // Set motor direction clockwise
+      digitalWrite(dirPin,HIGH); 
+  
+      // Spin motor one rotation slowly
+      for(int x = 0; x < STEPS_PER_REV; x++) {
+        digitalWrite(stepPin,HIGH); 
+        delayMicroseconds(1000); 
+        digitalWrite(stepPin,LOW); 
+        delayMicroseconds(1000); 
+      }
       break;
 
+      //Twist Blinds Closed
       case 16736925:
       digitalWrite(LED, LOW);
-      StepsRequired = - STEPS_PER_OUT_REV / 2;
-      steppermotor.setSpeed(400);
-      steppermotor.step(StepsRequired);
+      
+      // Set motor direction counterclockwise
+      digitalWrite(dirPin,LOW);
+  
+      // Spin motor two rotations quickly
+      for(int x = 0; x < (STEPS_PER_REV * 2); x++) {
+        digitalWrite(stepPin,HIGH);
+        delayMicroseconds(100);
+        digitalWrite(stepPin,LOW);
+        delayMicroseconds(1000);
+      }
       break;
     }
     irrecv.resume();
